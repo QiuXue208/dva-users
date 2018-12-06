f19bc7a2819013da40cf49085a512187fd690487
Snowy <1309571292@qq.com>
2018-12-06 16:10:02 +0800

    modify UsersList component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,8 +1,59 @@
 import React from 'react';
+import {Popconfirm,Table} from 'antd'
 
 function UsersList(){
+    const columns = [
+        {
+            title:'Name',
+            dataIndex:'name',
+            key:'name',
+            render:(text)=><a href="#">{text}</a>
+        },
+        {
+            title:'Age',
+            dataIndex:'age',
+            key:'age'
+        },
+        {
+            title:'Address',
+            dataIndex:'address',
+            key:'address'
+        },
+        {
+            title:'Action',
+            dataIndex:'action',
+            key:'action',
+            render:()=>{
+                return(
+                    <p>
+                        <a href="#">编辑</a>
+                        &nbsp;
+                        <Popconfirm title="Delete?" onConfirm={()=>{}}>
+                            <a href="#">删除</a>
+                        </Popconfirm>
+                    </p>
+                )
+            }
+        }
+    ]
+    const dataSource = [
+        {
+            key:'1',
+            name:'郭杰',
+            age:'44',
+            address:'华北',
+        },
+        {
+            key:'2',
+            name:'郭杰',
+            age:'44',
+            address:'华北',
+        }
+    ]
   return (
-    <div>Users List</div>
+    <div>Users List
+        <Table columns={columns} dataSource={dataSource}></Table>
+    </div>
   )
 }
 
