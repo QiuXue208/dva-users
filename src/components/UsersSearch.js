bc9ea97604dd808631188a1537e3ad4aa501d91a
Snowy <1309571292@qq.com>
2018-12-07 09:25:58 +0800

    modify it's paragraph

--- src/components/UsersSearch.js
+++ src/components/UsersSearch.js
@@ -1,8 +1,33 @@
 import React from 'react';
-
+import {Button,Input,Menu,Dropdown,Icon} from 'antd'
+import styles from './UsersSearch.css'
 function UsersSearch(){
+  const menu=(
+    <Menu>
+        <Menu.Item>
+          <a href="#">张三</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a href="#">李四</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a href="#">王五</a>
+        </Menu.Item>
+    </Menu>
+  )
   return (
-    <div>Users Search</div>
+    <div className={styles.search}>
+      <Button>
+          <Dropdown overlay={menu}>
+            <a className="ant-dropdown-link" href="#">
+              名字 <Icon type="down" />
+            </a>
+          </Dropdown>
+      </Button>
+      <Input className={styles.input} placeholder="please input name" />
+      <Button type="primary" >搜索</Button>
+      <Button className={styles.add}>添加</Button>
+    </div>
   )
 }
 
